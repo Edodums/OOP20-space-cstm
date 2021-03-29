43f3509f82c1d7dfbfcff9b61f5b9bff0020256c
Marco Edoardo Duma <marcoedoardo.duma@studio.unibo.it>
2021-03-30 01:07:25 +0200

    CommonShipView and EntitySprite interface created

---
+++ src/main/views/entities/CommonShipView.java
@@ -0,0 +1,30 @@
+package main.views.entities;
+
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.image.ImageView;
+import main.models.components.interfaces.Entity;
+import main.utils.Pair;
+import main.views.entities.interfaces.EntitySprite;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class CommonShipView implements EntitySprite {
+
+    @Override
+    public Node create(Map<Pair<Integer, Integer>, Optional<Entity>> commonShips) {
+        Group group = new Group();
+
+        commonShips.forEach((key, value) -> {
+            ImageView commonShip = new ImageView(value.get().getFileName());
+            // TODO: add unit ( 1 square/rectangle )
+            commonShip.setTranslateX(key.getX());
+            commonShip.setTranslateY(key.getY());
+
+            group.getChildren().add(commonShip);
+        });
+
+        return group;
+    }
+}
