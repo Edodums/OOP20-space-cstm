71c0049b6bfd1b3a78ada82d1c678f0ac3a72d08
mendi <mendi.kisha@studio.unibo.it>
2021-04-06 00:15:26 +0200

    Entity
    Added a new "move" method to Entity, so it could be helpful for both MotherShip and CommonShip classes.

--- src/main/models/components/interfaces/Entity.java
+++ src/main/models/components/interfaces/Entity.java
@@ -7,25 +7,76 @@ import java.util.Optional;
 
 import static main.models.Game.getMaxX;
 import static main.models.Game.getMaxY;
+import static main.models.components.entities.CommonShip.*;
+
+
 
 public interface Entity {
-    Map<Pair<Integer, Integer>, Optional<Entity>> create();
-    Pair<Integer, Integer> getPosition();
+    Map<Pair<Double, Double>, Optional<Entity>> create(Object newHashMap);
+    Pair<Double, Double> getPosition();
 
-    void move();
     void fire();
     void die();
 
-    Integer getSpawnNumber();
+    double getSpawnNumber();
 
-    /**
-     * TODO: check it and comment about it,if you think this is method more suitable for Player
-     * default boolean canEntityMove(Integer x, Integer y) {
-     *   return x < getMaxX() && y < getMaxY();
-     * }
-     */
+    default boolean canEntityMove(Double x, Double y) {
+       return x < getMaxX() && y < getMaxY();
+      }
+
 
     default boolean isNPC() {
         return this instanceof CommonShip;
     }
+
+    default void move() {
+        double unit = 1;
+        double accelerationFactor = getAccelerationFactor();
+        Pair<Double, Double> newPair = null;
+
+
+        /* 1. if you've reach the end you set take away 1 life from player counter. */
+        if (getPosition().getX().equals(getCommonEnemiesColumns()) &&
+                getPosition().getY().equals(getCommonEnemiesRows() + getCommonEnemiesNextRows())) {
+            //TODO: probably I'll handle it with an Event ( if so, @Arianna have to create an Event Manager )
+            System.out.println("REACHING END");
+            return;
+        }
+
+        /* 2. if you've to go right then check if the current number (x) is lower than the next one */
+        if (!isComingRight(getPosition().getY())) {
+            newPair = new Pair<>(getPosition().getX() + 1 + accelerationFactor, getPosition().getY());
+        }
+
+        /* 3. if you've to go left then check if the current number (x) is greater than the next one */
+        if (isComingRight(getPosition().getY())) {
+            newPair = new Pair<>(getPosition().getX() - 1 - accelerationFactor, getPosition().getY());
+        }
+
+        /* 4. if you reach the last column then go down and back (x - 1) && check if coming from the left */
+        if (getPosition().getX().equals(getCommonEnemiesColumns()) && !isComingRight(getPosition().getY())) {
+            newPair = new Pair<>(getPosition().getX() - 1 - accelerationFactor, getPosition().getY() + 1);
+        }
+
+        /* 5. if you reach the first column then go down and forth ( x + 1 ) && check if coming from the right */
+        if (getPosition().getX().equals(getStartingPoint().getX()) && isComingRight(getPosition().getY())) {
+            newPair = new Pair<>(getPosition().getX() + 1 + accelerationFactor, getPosition().getY() + 1);
+        }
+
+        if (newPair == null) {
+            throw new NullPointerException("Not really an expected scenario");
+        }
+
+    }
+
+    default double  getAccelerationFactor(){
+        Pair<Double, Double> pair = null;
+
+        return pair.getY() % getMaxX() * 0.25;
+    }
+
+    default boolean isComingRight(Double currentY) {
+        return currentY % 2 == 0;
+    }
+
 }
