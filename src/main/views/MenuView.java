7790f212519a83ca448f1f001bb56fc12af648ec
mendi <mendi.kisha@studio.unibo.it>
2021-04-17 18:37:37 +0200

    Menu
    Fixed some things on Menu and MenuView.

--- src/main/views/MenuView.java
+++ src/main/views/MenuView.java
@@ -1,42 +1,43 @@
 package main.views;
 
+import java.beans.PropertyChangeEvent;
+import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import main.controllers.GameController;
 import main.controllers.MenuController;
 import main.controllers.RankingController;
-import main.exceptions.SceneNotFound;
 import main.models.Game;
 import main.models.Menu;
 import main.models.Ranking;
 import main.utils.enums.CurrentScene;
 import main.utils.enums.Fxml;
 
-import java.awt.event.ActionEvent;
-import java.beans.PropertyChangeEvent;
-import java.io.IOException;
+public class MenuView implements View {
+    private static final String SCENE_PROPERTY = "currentScene";
+    private static final double BOUND_FACTOR = 1.8;
 
-public class MenuView extends View {
-    private final static double BOUND_FACTOR = 1.8;
-    private final Menu model;
-    private final MenuController controller;
+    private Menu model;
+    private MenuController controller;
 
     private Stage stage;
-    private Pane parent;
     private Scene scene;
+    private GridPane parent;
+
+    public MenuView() {}
 
     public MenuView(Menu model, MenuController controller, Stage stage) {
         this.model = model;
         this.controller = controller;
+        this.stage = stage;
 
         this.model.addPropertyChangeListener(this);
-
-        setStage(stage);
-        setCurrentSceneListener();
+        this.controller.setCurrentScene(CurrentScene.MENU);
     }
 
     @FXML
@@ -56,8 +57,8 @@ public class MenuView extends View {
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-        if (evt.getPropertyName().equals("currentScene")) {
-            setCurrentSceneListener();
+        if (evt.getPropertyName().equals(SCENE_PROPERTY)) {
+            setNewScene((CurrentScene) evt.getNewValue());
         }
     }
 
@@ -79,18 +80,16 @@ public class MenuView extends View {
         return this.scene;
     }
 
-    private String getParentFromCurrentScene(CurrentScene newValue) {
-        return newValue.toString().toLowerCase();
+    private String getFxmlFromCurrentScene(CurrentScene currentScene) {
+        return Fxml.valueOf(currentScene.toString()).toString();
     }
 
-    private View getView(CurrentScene scene) {
-        return switch (scene) {
+    private void getViewInstance(CurrentScene scene) {
+        switch (scene) {
             case GAME -> new GameView(new Game(), new GameController());
-            case MENU -> new MenuView(this.model, this.controller, this.stage);
-            case SETTINGS -> new SettingsView();
+            // case SETTINGS -> new SettingsView();
             case RANKING -> new RankingView(new Ranking(), new RankingController());
-            default -> throw new SceneNotFound(scene);
-        };
+        }
     }
 
     private void setUpdatedStage(Scene scene) {
@@ -98,25 +97,18 @@ public class MenuView extends View {
         this.stage.show();
     }
 
-    private void setCurrentSceneListener() {
-        this.model.addPropertyChangeListener("currentScene", event -> {
-            CurrentScene currentScene = (CurrentScene) event.getNewValue();
-            setParent(getParentFromCurrentScene(currentScene));
-            getView((CurrentScene) event.getNewValue());
-            setScene();
-            setUpdatedStage(getScene());
-        });
-    }
-
-    private void setStage(Stage stage) {
-        this.stage = stage;
+    private void setNewScene(CurrentScene newScene) {
+        setParent(getFxmlFromCurrentScene(newScene));
+        getViewInstance(newScene);
+        setScene();
+        setUpdatedStage(getScene());
     }
 
     void setScene() {
         this.scene = new Scene(this.parent, getWidth(), getHeight());
     }
 
-    final void setParent(String filename) {
+    private void setParent(String filename) {
         try {
             this.parent = FXMLLoader.load(getClass().getResource(filename));
         } catch (IOException e) {
