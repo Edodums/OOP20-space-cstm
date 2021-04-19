b4fb3716651cf50e1fadc63eb177e51b68376ab0
mendi <mendi.kisha@studio.unibo.it>
2021-04-19 22:55:58 +0200

    MotherShip
    Deleted javafx and view elements.

--- src/main/models/components/MotherShip.java
+++ src/main/models/components/MotherShip.java
@@ -1,12 +1,12 @@
 package main.models.components;
 
-import main.views.entities.interfaces.EntitySprite;
-import main.models.components.interfaces.Entity;
 import main.models.EntityImage;
+import main.models.components.interfaces.Collidable;
+import main.models.components.interfaces.Entity;
 import main.utils.Pair;
+
 import java.util.Map;
 import java.util.Optional;
-import javafx.scene.image.ImageView;
 
 
 public class MotherShip extends Collider implements Entity, Collidable {
