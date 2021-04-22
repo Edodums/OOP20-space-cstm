b8cd36967d34039ccd9840162188949f0712acd9
mendi <mendi.kisha@studio.unibo.it>
2021-04-22 23:07:50 +0200

    Added player Costumization Component

--- src/main/models/settings/CustomizeDefaults.java
+++ src/main/models/settings/CustomizeDefaults.java
@@ -1,19 +1,20 @@
 package main.models.settings;
 
+import main.utils.enums.Orientations;
+import main.utils.enums.ResourcePath;
+import main.utils.enums.WeaponType;
+
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.Objects;
 
-import main.utils.enums.Orientations;
-import main.utils.enums.ResourcePath;
-import main.utils.enums.WeaponType;
-
 public class CustomizeDefaults {
   private static final Orientations orientation = Orientations.VERTICAL;
   private static final TypeImage weaponNpc = new TypeImage(ResourcePath.IMAGES_PATH + "enemy_laser.png", WeaponType.NPC, new Grid(3,5,1,1));
   private static final TypeImage weaponPlayer = new TypeImage(ResourcePath.IMAGES_PATH + "player_laser.png", WeaponType.PLAYER, new Grid(1,8,1,1));
-  
+  private static final TypeImage entityPlayer = new TypeImage(ResourcePath.IMAGES_PATH + "default_player_sprite.png", EntityType.PLAYER, new Grid());
+
   private CustomizeDefaults() {}
   
   public static boolean areDefaultsNeeded(final Settings settings) {
