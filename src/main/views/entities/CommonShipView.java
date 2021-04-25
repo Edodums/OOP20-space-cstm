25319d68c50f053d9988c4fe9de730d65cf32ded
Marco Edoardo Duma <marcoedoardo.duma@studio.unibo.it>
2021-04-25 17:25:26 +0200

    updated Enemy views and EntitySprite interface

--- src/main/views/entities/CommonShipView.java
+++ src/main/views/entities/CommonShipView.java
@@ -1,32 +1,27 @@
 package main.views.entities;
 
-import java.util.Map;
-import java.util.Optional;
-import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.image.ImageView;
 import main.models.components.interfaces.Entity;
 import main.utils.Pair;
+import main.views.entities.interfaces.Animated;
 import main.views.entities.interfaces.EntitySprite;
 
-public class CommonShipView implements EntitySprite {
+public class CommonShipView implements EntitySprite, Animated  {
+  private ImageView commonShip;
   
   @Override
-  public Node create(Map<Pair<Double, Double>, Optional<Entity>> commonShips, double unit) {
-    Group group = new Group();
-    
-    commonShips.forEach((key, value) -> {
-      if (value.isEmpty()) {
-        return;
-      }
-      
-      ImageView commonShip = new ImageView(value.get().getFileName());
-      commonShip.setTranslateX(key.getX() + unit);
-      commonShip.setTranslateY(key.getY() + unit);
-      
-      group.getChildren().add(commonShip);
-    });
-    
-    return group;
+  public ImageView get() {
+    return this.commonShip;
+  }
+  
+  @Override
+  public void set(ImageView commonShip) {
+    this.commonShip = commonShip;
+  }
+  
+  @Override
+  public void create(Pair<Double, Double> position, Entity entity, double unit) {
+    EntitySprite.super.create(position, entity, unit);
+    animate(get());
   }
 }
