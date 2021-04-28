a69f7e80c33d0d19be312c80ee3e9fe00723e8db
Marco Edoardo Duma <marcoedoardo.duma@studio.unibo.it>
2021-04-28 18:28:55 +0200

    fixed creation of entites, add die implementation in mothership

--- src/main/models/components/entities/MotherShip.java
+++ src/main/models/components/entities/MotherShip.java
@@ -2,20 +2,23 @@ package main.models.components.entities;
 
 import java.util.Map;
 import java.util.Optional;
+import main.events.MotherShipHitEvent;
 import main.models.components.Collider;
 import main.models.components.interfaces.Collidable;
 import main.models.components.interfaces.Entity;
+import main.models.components.interfaces.Weapon;
 import main.models.settings.TypeImage;
 import main.utils.Pair;
+import org.greenrobot.eventbus.EventBus;
 
 
 public class MotherShip extends Collider implements Entity, Collidable {
 
-    private static final double POINTS = 100;
-    private static final double SPAWN_NUMBER = 1;
-    private static final double WIDTH = 4.0;
-    private static final double HEIGHT = 2.5;
-    private static final Pair<Double,Double> STARTING_POINT = new Pair<>(0.0, 0.0);
+    private static final float POINTS = 100;
+    private static final float SPAWN_NUMBER = 1;
+    private static final float WIDTH = 4.0f;
+    private static final float HEIGHT = 2.5f;
+    private static final Pair<Float, Float> STARTING_POINT = new Pair<>(0.0f, 0.0f);
 
     private final TypeImage typeImage;
 
@@ -25,32 +28,35 @@ public class MotherShip extends Collider implements Entity, Collidable {
     }
 
     @Override
-    public Map<Pair<Double, Double>, Optional<Entity>> create() {
-        return Map.of(STARTING_POINT, Optional.of(this));
+    public Map<Pair<Float, Float>, Optional<Entity>> create() {
+        final Pair<Float, Float> startingPosition = STARTING_POINT;
+        setStartingPosition(startingPosition);
+        
+        final Pair<Float, Float> position = new Pair<>(startingPosition.getX(), startingPosition.getY());
+        setPosition(position);
+        
+        return Map.of(position, Optional.of(this));
     }
 
     @Override
-    public double getWidth() {
+    public float getWidth() {
         return WIDTH;
     }
 
     @Override
-    public double getHeight() {
+    public float getHeight() {
         return HEIGHT;
     }
 
     @Override
-    public Pair<Double, Double> getPosition() {
+    public Pair<Float, Float> getPosition() {
         return STARTING_POINT;
     }
 
     @Override
-    public void fire() {
-        //empty
+    public void die(Weapon weapon) {
+        EventBus.getDefault().post(new MotherShipHitEvent(this, weapon));
     }
-
-    @Override
-    public void die() {System.out.println("Mothership is dead!");}
   
     @Override
     public TypeImage getTypeImages() {
@@ -58,17 +64,13 @@ public class MotherShip extends Collider implements Entity, Collidable {
     }
   
     @Override
-    public double getPointsValue() {
+    public float getPointsValue() {
         return POINTS;
     }
 
     @Override
-    public double getSpawnNumber() {
+    public float getSpawnNumber() {
         return SPAWN_NUMBER ;
-    }
-
-    public Pair<Double, Double> getStartingPoint() {
-        return STARTING_POINT;
     }
 }
 
