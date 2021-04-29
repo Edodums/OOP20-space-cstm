027b50b96aad1e76ac002087f095e20510800e75
Marco Edoardo Duma <marcoedoardo.duma@studio.unibo.it>
2021-04-29 23:27:13 +0200

    fixed CommonHitShip event

--- src/main/events/CommonShipHitEvent.java
+++ src/main/events/CommonShipHitEvent.java
@@ -1,14 +1,11 @@
 package main.events;
 
 import main.models.components.entities.CommonShip;
-import main.models.components.interfaces.Weapon;
 
 public class CommonShipHitEvent {
   public final CommonShip commonShip;
-  public final Weapon weaponToRemove;
   
-  public CommonShipHitEvent(CommonShip commonShip, Weapon weaponToRemove) {
+  public CommonShipHitEvent(final CommonShip commonShip) {
     this.commonShip = commonShip;
-    this.weaponToRemove = weaponToRemove;
   }
 }
