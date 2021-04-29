5c32010af7166f02bfbfc7fa266296c65b891614
Marco Edoardo Duma <marcoedoardo.duma@studio.unibo.it>
2021-04-29 22:42:13 +0200

    fixed events ( duplicastion and useless param )

--- src/main/events/MotherShipHitEvent.java
+++ src/main/events/MotherShipHitEvent.java
@@ -1,14 +1,11 @@
 package main.events;
 
 import main.models.components.entities.MotherShip;
-import main.models.components.interfaces.Weapon;
 
 public class MotherShipHitEvent {
   public final MotherShip motherShip;
-  public final Weapon weaponToRemove;
   
-  public MotherShipHitEvent(MotherShip motherShip, Weapon weaponToRemove) {
+  public MotherShipHitEvent(MotherShip motherShip) {
     this.motherShip = motherShip;
-    this.weaponToRemove = weaponToRemove;
   }
 }
