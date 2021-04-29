494b185be8aab7a75b74984b4672a8238605f8d4
Marco Edoardo Duma <marcoedoardo.duma@studio.unibo.it>
2021-04-29 23:23:14 +0200

    fixed Sprites

--- src/main/views/sprite/WeaponSprite.java
+++ src/main/views/sprite/WeaponSprite.java
@@ -4,13 +4,13 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import main.models.components.Collider;
 import main.models.components.interfaces.Weapon;
-import main.models.settings.Grid;
+import main.models.settings.interfaces.GridImage;
 import main.utils.Pair;
 
 
 public interface WeaponSprite extends Sprite {
   default void create(Pair<Float, Float> position, Weapon weapon, float unit) {
-    final Grid grid = weapon.getTypeImages().getGrid();
+    final GridImage grid = weapon.getTypeImages().getGrid();
     
     if (isSpriteDividedIntoGrid(grid)) {
       set(new ImageView(weapon.getFilename()));
