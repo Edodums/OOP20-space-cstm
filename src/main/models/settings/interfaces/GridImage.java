0d23495022a54a75e738d68b7171d1524bb907c5
il tuo nomeAriannaZ97 <53905536+AriannaZ97@users.noreply.github.com>
2021-04-30 21:32:12 +0200

    Modified interfaces of settings

--- src/main/models/settings/interfaces/GridImage.java
+++ src/main/models/settings/interfaces/GridImage.java
@@ -1,13 +1,18 @@
 package main.models.settings.interfaces;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import main.models.settings.Grid;
+
+@JsonDeserialize(as = Grid.class)
 public interface GridImage {
+    
     int getRows();
-
+    
     int getColumns();
-
+    
     int getSelectedColumn();
-
+    
     int getSelectedRow();
-
+    
     int getInternalColumns();
 }
